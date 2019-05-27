a4ca92c7c278086ce045509f5ad6743e285c05ed
wal <wal@qad.com>
2019-05-27 19:48:23 +0800

    Adding default options and other updates

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     py_modules=['teamselect'],
     install_requires=[
         'Click',
+        'pyyaml',
     ],
     entry_points='''
         [console_scripts]
--- teamselect.py
+++ teamselect.py
@@ -1,21 +1,44 @@
 """ Small utility to select x number of elements from a configurable array of values """
-
+import random
 import click
+import yaml
+
 
 def loadConfig():
-    pass
+    with open('options.yaml', 'r') as f:
+        return yaml.safe_load(f)
 
 
-def teamselect():
-    pass
+def saveConfig(config):
+    with open("options.yaml", 'w') as stream:
+        yaml.dump(config, stream)
+
+
+def teamselect(team, number, nodup):
+    conf = loadConfig()
+    members = conf.get('Teams').get(team)
+    alex = conf.get('Other').get('always-exclude')
+    chosen = dict()
+
+    while len(chosen) < number:
+        newitem = random.choice(list(members.keys()))
+        if newitem not in alex:
+            if nodup:
+                prev = conf.get('Other').get('previous-selection').keys()
+                if newitem not in prev:
+                    chosen[newitem] = members.get(newitem)
+                    del members[newitem]
+
+    click.echo(chosen)
+    conf['Other']['previous-selection'] = chosen
+    saveConfig(conf)
 
 @click.command()
 @click.argument('team')
 @click.option('--number','-n', default=1, help='Number of team members to choose.')
 @click.option('--nodup','-nd', is_flag=True, help='Exclude previousily selected team members.')
-
 def cli(team, number, nodup):
-    pass
+    teamselect(team, number, nodup)
 
 if __name__ == "__main__":
-    teamselect()
+    teamselect('SHRL', 3, True)
